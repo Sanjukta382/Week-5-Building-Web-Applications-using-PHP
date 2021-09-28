11a9204ace03d005779207b058abcce19eb04989
Sanjukta382 <66127954+Sanjukta382@users.noreply.github.com>
2021-09-28 11:10:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<title>SANJUKTA SEN PHP</title>
+<h1>SANJUKTA SEN PHP</h1>
+
+<p>
+    <?php
+    print "The SHA256 hash of \"SANJUKTA SEN\" is ".hash('sha256', 'SANJUKTA SEN');
+    ?>
+</p>
+
+<pre>
+ASCII ART:
+    ******
+    *
+    *
+    ******
+         *
+         *
+   *******
+
+</pre>
+
+<a href="fail.php">Click here to check the error setting</a>
+<br>
+<a href="check.php">Click here to cause a traceback</a>
